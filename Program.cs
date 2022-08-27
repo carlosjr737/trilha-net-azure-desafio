c920c5f7d4b6cb5c368fcfe083c1242d58d614ed
Carlos Antonio <carlosjr737@live.com>
2022-08-27 11:13:51 -0300

    Primeiro Commit

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ using TrilhaNetAzureDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<RHContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+builder.Services.AddDbContext<RHContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
